71b386993694434a099ae48058fa2e2c7f23a2b4
hosamhamdifl <hosamhamdifreelance@gmail.com>
2023-08-18 18:01:31 +0300

    BST

--- tree.py
+++ tree.py
@@ -4,36 +4,10 @@ class Node:
         self.left = None
         self.right = None
 
-    def search(self, target):
-        if self.data == target:
-            print("Found it!")
-            return self
-
-        if self.left and self.data > target:
-            return self.left.search(target)
-
-        if self.right and self.data < target:
-            return self.right.search(target)
-
-        print("Value is not in tree")
-
-    def add(self, data):
-        if self.data == data:
-            return
-
-        if data < self.data:
-            if self.left is None:
-                self.left = Node(data)
-            else:
-                self.left.add(data)
-
-        if data > self.data:
-            if self.right is None:
-                self.right = Node(data)
-            else:
-                self.right.add(data)
-
-        print("Value is not in tree")
+    def toStr(self):
+        if not self.isBalanced():
+            return str(self.data)+'*'
+        return str(self.data)
 
     def traversePreorder(self):
         print(self.data)
@@ -43,7 +17,6 @@ class Node:
             self.right.traversePreorder()
 
     def traverseInorder(self):
-
         if self.left:
             self.left.traverseInorder()
         print(self.data)
@@ -57,24 +30,58 @@ class Node:
             self.right.traversePostorder()
         print(self.data)
 
-    def height(self, h=0):
-        leftHeight = self.left.height(h+1) if self.left else h
-        rightHeight = self.right.height(h+1) if self.right else h
-        return max(leftHeight, rightHeight)
+    def search(self, target):
+        if self.data == target:
+            print("Found it!")
+            return self
+        
+        if self.left and self.data > target:
+            return self.left.search(target)
 
-    def getNodesAtDepth(self, depth, nodes=[]):
+        if self.right and self.data < target:
+            return self.right.search(target)
+
+        print("Value is not in tree")
+
+
+    def getNodesAtDepth(self, depth, nodes):
         if depth == 0:
-            nodes.append(self.data)
+            nodes.append(self)
             return nodes
+        
         if self.left:
             self.left.getNodesAtDepth(depth-1, nodes)
         else:
             nodes.extend([None]*2**(depth-1))
+        
         if self.right:
             self.right.getNodesAtDepth(depth-1, nodes)
         else:
             nodes.extend([None]*2**(depth-1))
         return nodes
+
+    def height(self, h=0):
+        leftHeight = self.left.height(h+1) if self.left else h 
+        rightHeight = self.right.height(h+1) if self.right else h
+        return max(leftHeight, rightHeight)
+
+    def add(self, data):
+        if data == self.data:
+            # Binary search tree does not contain duplicates
+            return
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+                return
+            else:
+                self.left.add(data)                
+        
+        if data > self.data:
+            if self.right is None:
+                self.right = Node(data)
+                return
+            else:
+                self.right.add(data)
 
     def findMin(self):
         if self.left:
@@ -88,45 +95,31 @@ class Node:
                 self.data = minimumValue.data
                 self.right = self.right.delete(minimumValue.data)
                 return self
-            else:
+            else: 
                 return self.right or self.left
-
+        
         if self.right and target > self.data:
-            self.right = self.right.delete(target)
+                self.right = self.right.delete(target)
 
         if self.left and target < self.data:
-            self.left = self.left.delete(target)
+                self.left = self.left.delete(target)
         return self
 
+    def isBalanced(self):
+        leftHeight = self.left.height()+1 if self.left else 0
+        rightHeight = self.right.height()+1 if self.right else 0
+        return abs(leftHeight - rightHeight) < 2
 
 class Tree:
     def __init__(self, root, name=''):
         self.root = root
         self.name = name
 
-    def search(self, target):
-        return self.root.search(target)
-
-    def traversePreorder(self):
-        self.root.traversePreorder()
-
-    def traverseInorder(self):
-        self.root.traverseInorder()
-
-    def traversePostorder(self):
-        self.root.traversePostorder()
-
-    def height(self):
-        return self.root.height()
-
-    def getNodesAtDepth(self, depth):
-        return self.root.getNodesAtDepth(depth)
-
     def _nodeToChar(self, n, spacing):
         if n is None:
             return '_'+(' '*spacing)
-        spacing = spacing-len(str(n))+1
-        return str(n)+(' '*spacing)
+        spacing = spacing-len(n.toStr())+1
+        return n.toStr()+(' '*spacing)
 
     def print(self, label=''):
         print(self.name+' '+label)
@@ -138,95 +131,41 @@ class Tree:
         for depth in range(0, height+1):
             if depth > 0:
                 # print directional lines
-                print(' '*(offset+1) + (' '*(spacing+2)
-                                        ).join(['/' + (' '*(spacing-2)) + '\\']*(2**(depth-1))))
+                print(' '*(offset+1)  + (' '*(spacing+2)).join(['/' + (' '*(spacing-2)) + '\\']*(2**(depth-1))))
             row = self.root.getNodesAtDepth(depth, [])
-            print((' '*offset) +
-                  ''.join([self._nodeToChar(n, spacing) for n in row]))
+            print((' '*offset)+''.join([self._nodeToChar(n, spacing) for n in row]))
             spacing = offset+1
             offset = int(offset/2) - 1
         print('')
 
-    def add(self, data):
-        return self.root.add(data)
+    def traverseInorder(self):
+        self.root.traverseInorder()
 
-    def delete(self, target):
-        self.root = self.root.delete(target)
+    def traversePreorder(self):
+        self.root.traversePreorder()
 
+    def traversePostorder(self):
+        self.root.traversePostorder()
 
-node = Node(10)
+    def search(self, target):
+        return self.root.search(target)
 
-node.left = Node(5)
-node.right = Node(15)
+    def getNodesAtDepth(self, depth):
+        return self.root.getNodesAtDepth(depth)
 
-node.left.left = Node(2)
-node.left.right = Node(6)
+    def height(self):
+        return self.root.height()
 
-node.right.left = Node(13)
-node.right.right = Node(10000)
+    def add(self):
+        self.root.add()
 
-myTree = Tree(node, 'Ryan\'s Tree')
-
-
-found = myTree.search(10000)
-print(found.data)
-
-tree = Tree(Node(50), 'Tree Traversals')
+tree = Tree(Node(50))
 tree.root.left = Node(25)
 tree.root.right = Node(75)
-tree.root.left.left = Node(10)
-tree.root.left.right = Node(35)
-tree.root.left.right.left = Node(30)
-tree.root.left.right.right = Node(42)
-tree.root.left.left.left = Node(5)
-tree.root.left.left.right = Node(13)
-
-tree = Tree(Node(50), 'A Very Tall Tree')
-tree.root.left = Node(25)
-tree.root.right = Node(75)
-tree.root.left.left = Node(10)
-tree.root.left.right = Node(35)
-tree.root.left.right.left = Node(30)
-tree.root.left.right.right = Node(42)
-tree.root.left.left.left = Node(5)
-tree.root.left.left.right = Node(13)
-tree.root.left.left.left.left = Node(2)
-
-print("\n In Order")
-print(tree.traverseInorder())
-
-print("\n Post Order")
-print(tree.traversePostorder())
-
-print("\n Pre Order")
-print(tree.traversePreorder())
-
-print(tree.height())
-
-tree = Tree(Node(50), "My Tree")
-tree.root.left = Node(25)
-tree.root.right = Node(75)
-tree.root.left.left = Node(13)
-tree.root.left.right = Node(35)
-tree.root.left.right.right = Node(37)
-tree.root.right.left = Node(55)
-tree.root.right.right = Node(103)
-tree.root.left.left.left = Node(2)
-tree.root.left.left.right = Node(20)
-tree.root.right.left = Node(55)
-tree.root.right.right.right = Node(256)
-
-tree.add(2)
-tree.add(75)
-tree.add(76)
-tree.add(257)
-tree.add(99)
-tree.add(1)
-
-tree.delete(1)
-tree.delete(50)
-tree.delete(99)
-tree.delete(257)
-
+tree.root.right.right = Node(100)
+tree.root.right.right.right = Node(150)
 
 tree.print()
+
+print(tree.root.isBalanced())
+print(tree.root.left.isBalanced())
