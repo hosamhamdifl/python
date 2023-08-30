3636f34b090c5a210523b5b752078a0f56efc8ae
hosamhamdifl <hosamhamdifreelance@gmail.com>
2023-08-30 08:20:15 +0300

    numpy min max

--- numpy_prod_sum.py
+++ numpy_prod_sum.py
@@ -1,13 +1,10 @@
 import numpy as np
 
-shape=tuple(map(int,input().split()))
-arrayA=[]
+shape = tuple(map(int, input().split()))
+arrayA = []
 for _ in range(shape[0]):
-    row = list(map(int,input().split()))
+    row = list(map(int, input().split()))
     arrayA.append(row)
-arrayB=np.reshape(arrayA,shape)
-arrayC=np.sum(arrayB,axis=0)
+arrayB = np.reshape(arrayA, shape)
+arrayC = np.sum(arrayB, axis=0)
 print(np.prod(arrayC))
-    
-
-
