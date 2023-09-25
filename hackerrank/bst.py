31264da143c5abe6efe6cafbdcc957de013d7cf2
hosamhamdifl <hosamhamdifreelance@gmail.com>
2023-09-25 03:50:18 +0300

    hackerrank

---
+++ hackerrank/bst.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root == None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def getHeight(self, root):
+        # Write your code here
+        if root is None:
+            return -1
+        else:
+            l = self.getHeight(root.left)
+            r = self.getHeight(root.right)
+            return 1+max(l, r)
+
+
+T = int(input())
+myTree = Solution()
+root = None
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root, data)
+height = myTree.getHeight(root)
+print(height)
